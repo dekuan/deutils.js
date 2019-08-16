cac7e372c624d8434a329f9ddfd4ccf962c57c9b
liuqixing <liuqixing@gmail.com>
2019-08-16 12:34:44 +0800

    Release version 1.0.10 Fixed a bug about index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { DeUtilsCore }		= require( './src/deutils.core.js' );
 const { DeUtilsNetwork }	= require( './src/deutils.network.js' );
 const { DeUtilsWait }		= require( './src/deutils.wait' );
+const { DeUtilsTime }		= require( './src/deutils.time' );
 
 
 
@@ -13,4 +14,5 @@ module.exports	=
 	DeUtilsCore	: DeUtilsCore,
 	DeUtilsNetwork	: DeUtilsNetwork,
 	DeUtilsWait	: DeUtilsWait,
+	DeUtilsTime	: DeUtilsTime,
 };

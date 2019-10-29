69ba7f4db712ffe1c81f71ee89ac897911d392f1
liuqixing <liuqixing@gmail.com>
2019-10-29 10:12:11 +0800

    Added build-dist.sh

---
+++ build-dist.sh
@@ -0,0 +1,5 @@
+browserify "src/deutils.cluster.js" -o "./dist/deutils.cluster.br.js"
+browserify "src/deutils.core.js" -o "./dist/deutils.core.br.js"
+browserify "src/deutils.network.js" -o "./dist/deutils.network.br.js"
+browserify "src/deutils.time.js" -o "./dist/deutils.time.br.js"
+browserify "src/deutils.wait.js" -o "./dist/deutils.wait.br.js"
